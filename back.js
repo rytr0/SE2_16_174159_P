e43a1d9e00859b88a909e3cb8d1a107f0063e010
armaniv <valearmani95@gmail.com>
2016-11-30 21:57:38 +0100

    sviluppo 3

--- back.js
+++ back.js
@@ -4,13 +4,21 @@ notizie.push(new Notizia(1, "Hot", "Trento premiata città verde", "Grazie al pa
 
 notizie.push(new Notizia(2, "Hot", "Trento SmartCity", "Grazie all'amministrazione digitale oggi è possibile consultare un referto medico online, oppure pagare il bollo con lo smartphone."));
 
-notizie.push(new Notizia(3, "Normal", "Calcio", "Trento vs Verona allo stadio"));
+notizie.push(new Notizia(3, "Notizie città", "Match importante", "Allo stadio Briamasco si terrà l'attesissima partita Trento vs Verona. Vi diamo appuntamento alle 20:30"));
 
 notizie.push(new Notizia(4, "Hot", "Archivio Fotografico Storico", "La mostra sarà inaugurata giovedì 1 dicembre alle ore 17.30 presso la sede di Torre Vanga, piazza della Portèla 1."));
 
 notizie.push(new Notizia(5, "Hot", "Vota!", "Fai la tua scelta tra si e no!"));
 
+notizie.push(new Notizia(6, "Notizie città", "Sempre più svago", "Divertiti a Trento! Durante il periodo invernale è stata allestita in piazza Dante una fantastica pista di pattinaggio "));
 
+notizie.push(new Notizia(7, "Notizie città", "Tutti in piazza", "Divertiamoci assieme! Per la sera del 25 Dicembre non perderti in piazza del Duomo la fantastica festa che abbiamo organizzato per te!"));
+
+notizie.push(new Notizia(8, "Notizie Università", "Convegno di Chimica", "L'università presenta una lezione aperta al pubblico riguardo la struttura delle molecole. Sarà per il dottor Franco"));
+
+notizie.push(new Notizia(9, "Notizie Università", "Convegno di Fisica", "L'università è lieta di presentare un dibattito riguardo i campi elettrici. L'oratore sarà la dottoressa Anna"));
+
+notizie.push(new Notizia(10, "Notizie Università", "Convegno di Filosofia", "L'università presenta una lezione con argomento il pensiero di Platone. Sarà presentata del dottor Marco e sarà aperta al pubblico"));
 
 function Notizia(id, tipo, titolo, testo)
 {
@@ -22,7 +30,7 @@ function Notizia(id, tipo, titolo, testo)
 
 
 
-function StampaNotiziHot()
+function StampaNotizie(tiposcelto)
 {
     //imopstiamo il limite a 3, appunto per scopi puramente di sviluppo di questa versione web, nell'app definitiva NON ci dovrà essere
     var conta=0,limite=3;
@@ -31,7 +39,7 @@ function StampaNotiziHot()
     
     for (var i=0; i<notizie.length; i++)
     {
-        if (notizie[i].tipo=="Hot" && conta<limite)
+        if (notizie[i].tipo==tiposcelto && conta<limite)
         {
             temp.push(notizie[i]);
             conta++;
@@ -43,4 +51,4 @@ function StampaNotiziHot()
 }
 
 
-exports.StampaNotiziHot = StampaNotiziHot;
+exports.StampaNotizie = StampaNotizie;
--- index.js
+++ index.js
@@ -7,11 +7,89 @@ var bodyParser = require('body-parser');
 
 app.listen(1337, "127.0.0.1");
 app.use(express.static(__dirname + '/Scripts'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.post('/CaricaSezione', function(req, res)
+    {
+        if (req.body.Sez2=="Notizie Città" || req.body.Sez3=="Notizie Città")
+        {
+            
+            var notizie = back.StampaNotizie("Notizie città");
+            
+            bind.toFile('index.tpl', 
+            {
+                Sezione: "Notizie Città",
+                titolo1:notizie[0].titolo,
+                notizia1:notizie[0].testo,
+                titolo2:notizie[1].titolo,
+                notizia2:notizie[1].testo,
+                titolo3:notizie[2].titolo,
+                notizia3:notizie[2].testo,
+                Sezione2: "Hot",
+                Sezione3: "Notizie Università"
+            }, 
+            function(data) 
+            {
+                res.writeHead(200,{'Content-Type': 'text/html'});
+                res.end(data);
+            });
+        }
+        
+        else if (req.body.Sez2=="Notizie Università" || req.body.Sez3=="Notizie Università")
+        {
+            
+            var notizie = back.StampaNotizie("Notizie Università");
+            
+            bind.toFile('index.tpl', 
+            {
+                Sezione: "Notizie Università",
+                titolo1:notizie[0].titolo,
+                notizia1:notizie[0].testo,
+                titolo2:notizie[1].titolo,
+                notizia2:notizie[1].testo,
+                titolo3:notizie[2].titolo,
+                notizia3:notizie[2].testo,
+                Sezione2: "Hot",
+                Sezione3: "Notizie Città"
+            }, 
+            function(data) 
+            {
+                res.writeHead(200,{'Content-Type': 'text/html'});
+                res.end(data);
+            });
+        }
+        else
+        {
+            var notizie = back.StampaNotizie("Hot");
+    
+            bind.toFile('index.tpl', 
+            {
+                Sezione: "Hot",
+                titolo1:notizie[0].titolo,
+                notizia1:notizie[0].testo,
+                titolo2:notizie[1].titolo,
+                notizia2:notizie[1].testo,
+                titolo3:notizie[2].titolo,
+                notizia3:notizie[2].testo,
+                Sezione2: "Notizie Città",
+                Sezione3: "Notizie Università"
+
+            }, 
+            function(data) 
+            {
+                res.writeHead(200,{'Content-Type': 'text/html'});
+                res.end(data);
+            });  
+        }
+            
+    });
+           
 
 
 app.use('/', function(req, res)
     {    
-        var notizie = back.StampaNotiziHot();
+        var notizie = back.StampaNotizie("Hot");
     
         bind.toFile('index.tpl', 
         {
@@ -22,7 +100,7 @@ app.use('/', function(req, res)
             notizia2:notizie[1].testo,
             titolo3:notizie[2].titolo,
             notizia3:notizie[2].testo,
-            Sezione2: "Notizi Città",
+            Sezione2: "Notizie Città",
             Sezione3: "Notizie Università"
             
         }, 
